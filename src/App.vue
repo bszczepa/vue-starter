040f0d93cdc8b41edbcb93344a4465c9136e9700
Bartek <bar.szczepanski>
2020-04-25 15:43:54 +0200

    computed property

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
       <h1> Witaj w systemie do zapisów na zajęcia </h1>
       <div v-if="authenticatedUsername == ''">
-          <login-form @login="logMeIn($event)"></login-form>
+          <login-form @login="logMeIn($event)" button-label="Zalooooguj się"></login-form>
       </div>
       <div v-else>
           <h2>Witaj {{authenticatedUsername}}!</h2>
